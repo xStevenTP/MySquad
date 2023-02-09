3d1a2c54eeb2ecbe59ee1b986c870b26fc550dbb
Steven Pham <steventuanpham@gmail.com>
2023-02-08 22:03:11 -0500

    Cleaning up app code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
