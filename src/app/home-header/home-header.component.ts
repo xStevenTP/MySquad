8c905b19f566cf44e0dfa3ab9efd1570942179ec
Steven Pham <steventuanpham@gmail.com>
2023-02-08 22:02:51 -0500

    Create HomePage Header Component

---
+++ src/app/home-header/home-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home-header',
+  templateUrl: './home-header.component.html',
+  styleUrls: ['./home-header.component.css']
+})
+export class HomeHeaderComponent {
+  @Input()
+  currentTeam!: String;
+
+}
