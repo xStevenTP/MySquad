283df203ee11cea46e5a0456e80efb7455438af4
Steven Pham <steventuanpham@gmail.com>
2023-02-12 13:12:36 -0500

    Adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
+import { AddTeamComponent } from './add-team/add-team.component';
+import { TeamMatchesComponent } from './team-matches/team-matches.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeHeaderComponent
+    HomeHeaderComponent,
+    AddTeamComponent,
+    TeamMatchesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
